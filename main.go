0f99595ddbfa51b1d3a1c4ae922fec45b58ed1c1
ljsnell <ljsnell7@gmail.com>
2024-09-18 12:28:08 -0500

    Update main.go
    Leftover.

--- main.go
+++ main.go
@@ -11,14 +11,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
 type Albums struct {
 	Albums []Album `json:"albums"`
 }
