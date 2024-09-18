d105bcedca6eb2598aac7cef20cd12673418465a
ljsnell <ljsnell7@gmail.com>
2024-09-18 10:41:04 -0500

    Update main.go
    Cleanup

--- main.go
+++ main.go
@@ -19,13 +19,6 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
-// albums slice to seed record album data.
-//
-//	var albums = []album{
-//		{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-//		{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-//		{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-//	}
 type Albums struct {
 	Albums []Album `json:"albums"`
 }
@@ -43,7 +36,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	// router.GET("/albums/:id", getAlbumByID)
-	// router.POST("albums", postAlbums)
+
 	router.Run(":8080")
 }
 
@@ -66,21 +59,6 @@ func getAlbums(c *gin.Context) {
 	fmt.Println(albums.Albums)
 	c.IndentedJSON(http.StatusOK, albums.Albums)
 }
-
-// postAlbums adds an album from JSON received in the request body.
-// func postAlbums(c *gin.Context) {
-// 	var newAlbum album
-
-// 	// Call BindJSON to bind the received JSON to
-// 	// newAlbum.
-// 	if err := c.BindJSON(&newAlbum); err != nil {
-// 		return
-// 	}
-
-// 	// Add the new album to the slice.
-// 	albums = append(albums, newAlbum)
-// 	c.IndentedJSON(http.StatusCreated, newAlbum)
-// }
 
 // // getAlbumByID locates the album whose ID value matches the id
 // // parameter sent by the client, then returns that album as a response.
